5a9ac918079ea775865990770040178a14e86b45
Sami <59291934+letmetest1822-sami@users.noreply.github.com>
2019-12-31 00:10:55 +0300

    Add files via upload

---
+++ day_2_print/Question6.java
@@ -0,0 +1,27 @@
+package assignments.day_2_print;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		System.out.println("\t   ^    ");
+		System.out.println("\t  / \\ ");
+		System.out.println("\t /   \\ ");
+		System.out.println("\t/     \\ ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t|     | ");
+		System.out.println("\t------- ");
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
