b2d72c7cdfae899ed83e92f8a2c9e4b6a41eefef
Sami <59291934+letmetest1822-sami@users.noreply.github.com>
2020-02-27 23:59:01 +0300

    Add files via upload

--- Assignment11/Question51_innerAndOuter.java
+++ Assignment11/Question51_innerAndOuter.java
@@ -1,5 +1,7 @@
 package assignments.Assignment11;
 
+import java.util.Arrays;
+
 public class Question51_innerAndOuter {
 
 	public static void main(String[] args) {
@@ -17,7 +19,30 @@ Sample Output:
      input (inner): 6, 5
      output: false
 		 */
-
+		int outer [] = {1, 2, 4, 6};
+		int inner [] = {2, 4};
+		
+		int outer2 [] = {1, 2, 4};
+		int inner2 [] = {6, 5};
+		
+		innerAndOuter(outer, inner);
+		innerAndOuter(outer2, inner2);
 	}
-
+	public static void innerAndOuter(int [] arr1 ,int [] arr2  ) {
+		
+		for(int i=0; i<arr1.length;i++) {
+			
+			if(arr2.length<arr1.length) {
+				
+				if((Arrays.binarySearch(arr1, arr2[i]))>0){
+					System.out.println(true);
+					break;
+				}else 
+					{System.out.println(false);
+					break;
+				}
+			}
+		}
+	}
+	
 }
