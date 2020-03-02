028e697fb95e0c82ed64ec77fb50ded9ef94bde9
Sami <59291934+letmetest1822-sami@users.noreply.github.com>
2020-03-02 23:24:30 +0300

    Add files via upload

--- Assignment13/Question78_reverseVowels.java
+++ Assignment13/Question78_reverseVowels.java
@@ -1,10 +1,13 @@
 package assignments.Assignment13;
 
+import java.util.ArrayList;
+
 public class Question78_reverseVowels {
 
 	public static void main(String[] args) {
 		/*
-		 * Reverse a vowels of string without affecting any other characters. Write a method reverseVowels() that will return a string with reversed vowels.
+		 * Reverse a vowels of string without affecting any other characters. 
+		 * Write a method reverseVowels() that will return a string with reversed vowels.
 
 Sample Output:
      Input: str = "apple"
@@ -18,7 +21,39 @@ Sample Output:
 
 Note that only vowels were reversed.
 		 */
-
+		String input = "apple";
+		
+		System.out.println(reverseVowels(input));
+		
 	}
-
+	public static String reverseVowels(String str) {
+		
+		ArrayList<Integer> vowelIndex = new ArrayList<>();
+		
+		for (int i = 0; i< str.length(); i++) {
+			
+			if ( (str.substring(i, i+1).equalsIgnoreCase("a")) || (str.substring(i, i+1).equalsIgnoreCase("e")) || (str.substring(i, i+1).equalsIgnoreCase("i")) || (str.substring(i, i+1).equalsIgnoreCase("u")) || (str.substring(i, i+1).equalsIgnoreCase("o"))){
+				vowelIndex.add(str.indexOf(str.substring(i, i+1)));
+			}
+		}
+		String swap = "";
+		
+		int j = vowelIndex.size()-1;
+		
+		int k = 0;
+		
+		for (int i = 0 ; i<str.length(); i++) {
+			
+			if(i == vowelIndex.get(k)) {
+				
+				swap += str.charAt(vowelIndex.get(j));
+				
+				k++; j--;
+			}
+			else {
+				swap += str.charAt(i);
+			}
+		}
+	return swap;
+	}
 }
--- Assignment13/Question79_scalarMultiplication.java
+++ Assignment13/Question79_scalarMultiplication.java
@@ -1,10 +1,14 @@
 package assignments.Assignment13;
 
+import java.util.Arrays;
+
 public class Question79_scalarMultiplication {
 
 	public static void main(String[] args) {
 		/*
-		 * Scalar multiplication of matrix is the simplest and easiest way to multiply matrix . Basically you get an int 2d array and need to multiply each of its elements by the value of parameter n int. And return the same 2D array back.
+		 * Scalar multiplication of matrix is the simplest and easiest way to multiply matrix .
+		 * Basically you get an int 2d array and need to multiply each of its elements by the value of parameter n int.
+		 * And return the same 2D array back.
 
 Sample Output:
 
@@ -44,6 +48,29 @@ returns:
 hint: use two nested for loops
 		 */
 
+		int [] [] inputMatrix1 = {{1,1,1},{1,1,2}};
+		int [] [] inputMatrix2 = {{2,3,5},{1,1,2}};
+		
+		int inputMultiplier1 = 5;
+		int inputMultiplier2 = 2;
+		
+		System.out.println(Arrays.deepToString(inputMatrix1));
+		System.out.println(Arrays.deepToString(scalar(inputMatrix1,inputMultiplier1)));
+		
+		System.out.println("*********");
+		
+		System.out.println(Arrays.deepToString(inputMatrix2));
+		System.out.println(Arrays.deepToString(scalar(inputMatrix2,inputMultiplier2)));
 	}
-
+	public static int [][] scalar (int [] [] matrix, int multiplier){
+		
+		for (int i = 0; i<matrix.length;i++) {
+			
+			for(int j=0;   j<matrix[i].length;   j++) {
+				
+				matrix[i][j] = matrix[i][j] * multiplier;
+			}
+		}
+		return matrix;
+	}
 }
