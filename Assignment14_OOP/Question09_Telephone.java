4244dac3969e29d985646df0f322a2d25fb31d8b
Sami <59291934+letmetest1822-sami@users.noreply.github.com>
2020-04-21 13:40:24 +0300

    Add files via upload

---
+++ Assignment14_OOP/Question09_Telephone.java
@@ -0,0 +1,40 @@
+package assignments.Assignment14_OOP;
+
+public class Question09_Telephone {
+
+/*
+ * Write the definition of a class Telephone.
+ * The class has no constructors, 
+ * one instance variable of type String called number, and 
+ * two public static variables.
+ * One is of type int called quantity, initialized to 250;
+ * the other is of type double called total, initialized to 15658.92.
+ * Create getNumber() and setNumber() methods that will manipulate with number instance variable.
+ */
+	
+	//variables
+	String number;
+	public static int quantity = 250;
+	public static double total = 1565.92;
+	
+	//getter
+	public String getNumber() {
+		return number;
+	}
+
+	//setter
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	//create a main method to run the code
+	public static void main(String[] args) {
+
+		Question09_Telephone Tel = new Question09_Telephone();
+		
+		System.out.println(Tel.getNumber());				//150
+		Tel.setNumber("150");
+		System.out.println(Tel.getNumber());				//150
+		
+	}
+}
